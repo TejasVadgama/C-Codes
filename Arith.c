2d434e2bb197f8d856b5db14bbd66a8d6030d612
Tejas <tejasvadgamak.0110@gmail.com>
2025-02-04 13:21:42 +0530

    2nd Day.

---
+++ Arith.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main() {
+    int A = 20;
+    int B = 10;
+
+    printf("This Is the Arithmetic Operations in C. \n");
+    
+    printf("Addition of A & B is: %d\n",A + B);
+    printf("Subtraction of A & B is: %d\n",A - B);
+    printf("Multiplication of A & B is: %d\n",A * B);
+    printf("Division of A & B is: %d\n",A / B);
+
+    return 0;
+}
