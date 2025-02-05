842aff0dac712b26e8e153adfcfc180d07d21945
Tejas <tejasvadgamak.0110@gmail.com>
2025-02-05 17:26:18 +0530

    4th Day

---
+++ Relational.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5, c = 10;
+
+    printf("a > b: %d\n", a > b); 
+    printf("a < b: %d\n", a < b);
+    printf("a == c: %d\n", a == c);
+    printf("a != b: %d\n", a != b);
+    printf("a <= b: %d\n", a <= b);
+    printf("a >= b: %d\n", a >= b); 
+
+    return 0;
+}
