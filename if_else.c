4f01589f7793e31bef11c658fe9f37113fd2ccce
Tejas <tejasvadgamak.0110@gmail.com>
2025-02-07 18:43:27 +0530

    6th Day..

---
+++ if_else.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int i = true; //Here initalized the I Variable.
+
+    if (i == true) // Check Condition Value if I True then Print inner Printf Function.
+    {
+        printf("i Value is True.");
+    }else
+    {
+        printf("i Value is the False."); // if i false the condition then print else Statement.
+    }
+    return 0;
+}
