837b2286e9a1d67ace41ef31d2ba3d0b705824d2
Tejas <tejasvadgamak.0110@gmail.com>
2025-02-07 18:37:36 +0530

    6th Day..

---
+++ Const.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main() {
+    const A = 15; //Const Variables Are Always print the Same Value Without Change it.
+
+    A = 10; // Try to change it. but it Will Print Define Const Value Always.
+    
+    printf("%d/n",A);
+}
