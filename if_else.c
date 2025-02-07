7c3d4452dd6a7d03aaf2f8bb5430d0bf73871bbf
Tejas <tejasvadgamak.0110@gmail.com>
2025-02-07 18:51:01 +0530

    6th Day..updated Code.

--- if_else.c
+++ if_else.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 
 int main() {
-    int i = true; //Here initalized the I Variable.
+    int i = 1; //Here initalized the I Variable.
 
-    if (i == true) // Check Condition Value if I True then Print inner Printf Function.
+    if (i == 1) // Check Condition Value if I True then Print inner Printf Function.
     {
-        printf("i Value is True.");
+        printf("i Value is: %d\n",i);
+    }else if (i == 0) //if condition false then The 2nd Is elif Condition Check If true then execute that condition.
+    {
+     printf("i value is: %d\n",i);
     }else
     {
-        printf("i Value is the False."); // if i false the condition then print else Statement.
+        printf("i Has Undefined Value."); // if i false the condition then print else Statement.
     }
     return 0;
 }
