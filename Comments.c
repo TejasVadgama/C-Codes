f6c3c13040e5ee91a89fea5f9f4a84e91c31383b
Tejas <tejasvadgamak.0110@gmail.com>
2025-02-19 13:37:08 +0530

    Continue To C-Codes Journey

---
+++ Comments.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+    //This is Normal Comment Done By "//" Sign.
+    Printf("Or Also You Can Take Cursor To Line And Press Ctrl + / That Make Line TO Cursor.");
+    /*Now For Multiple We Use "/*" Sign 
+    That Comment as Line We Want.*/
+    Print("")
+}
